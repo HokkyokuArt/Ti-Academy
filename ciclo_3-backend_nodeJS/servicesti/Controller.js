bf4fca29ccd47514330d603df51310990c803153
HokkyokuArt <jeanartico13@hotmail.com>
2021-08-31 17:42:39 -0300

    ciclo 3 aula 5

--- ciclo_3-backend_nodeJS/servicesti/Controller.js
+++ ciclo_3-backend_nodeJS/servicesti/Controller.js
@@ -74,6 +74,7 @@ app.get('/servico/:id', async(req,res)=>{
 
 // get = importar da base de da dados / linha de aplicação
 // post = inserção via formulário
+//put = 
 
 //1 visualize todos os clientes
 app.get('/listacliente', async(req,res)=>{
@@ -88,7 +89,7 @@ app.get('/listacliente', async(req,res)=>{
 //2 visualize os clientes em ordem de antiguidade
 app.get('/ordenarclientes', async(req,res)=>{
     await cliente.findAll({
-        order:[['id','ASC']]
+        order:[['id','DESC']]
     }).then(function(clientes){
         res.json({clientes})
     });
@@ -133,26 +134,206 @@ app.get('/qtdpedidos', async(req,res)=>{
   
 });
 
-//desafio
+app.get('/atualizaservico', async(req,res)=>{
+    await servico.findByPk(2) // comando para buscar a informaçao no servidor
+    .then(servico=>{
+        servico.nome='HTML/CSS/JS';
+        servico.descricao='Páginas estáticas e dinâmicas estilizadas';
+        servico.save(); // comando que salva as atualizaçoes no servidor
+        return res.json({servico});
 
-/* app.get('/listaclientes/:nome', async(req,res)=>{
-    cliente.findByPk(req.params.nome)
-    .then(listaclientes =>{
-        return res.json(
-            await pedido.sum('valor')
-            // await pedido.sum('valor', { where: { cliente: { [Op.gt]: 1} } }),
-        )
+    });
+});
+
+// put  
+// IMPORTANTE: editar um serviço por vez pq se não, não vai atualizar no servidor
+app.put('/editarservico', (req,res)=>{  //nao é async pq precisa passar pela requisição
+    servico.update(req.body,{
+        where: {id: req.body.id}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: "Serviço foi alterado com sucesso."
+        });
     }).catch(function(erro){
         return res.status(400).json({
-            error:true,
-            messagem: "Cliente não está existe."
+            error: true,
+            message: "Erro na alteração do serviço."
         });
     });
+});
 
-}); */
+app.get('/servicospedidos', async(req,res)=>{
+    await servico.findByPk(2, {
+        include:[{all:true}]
+    }).then(servico=>{
+        return res.json({servico});
+    });
+
+}); 
+
+app.put('/editarpedido', (req,res)=>{
+    pedido.update(req.body,{
+        where: {ServicoId: req.body.ServicoId}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: "Pedido modificado com sucesso."
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Não foi possível modificar o pedido."
+        });
+    });
+});
 
 
 
+// 1 faça busca por serviços de cliente passando o id do cliente
+//   no corpo da requisição
+app.get('/listaservicocliente/:id', async (req, res)=>{     
+   
+    await pedido.findAll({ where: { ClienteId: [req.params.id]  } })
+    .then(function(pedidos){
+        res.json(pedidos)        
+    });
+    console.log(pedidos,valor,ClienteId)
+    });
+
+
+/* app.get('/clienteservico', (req,res)=>{  //nao é async pq precisa passar pela requisição
+    pedido.findOne(req.body,{
+        where: {ClienteId: req.body.ClienteId}
+    }).then(function(){
+        return res.json({
+            error: false,
+            servicos
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Erro."
+        });
+    });
+});
+ */
+
+
+
+// 2 Utilize a rota para consultar clientes e faça a edição de um
+//   cliente pelo método put.
+
+//consultar: /listacliente
+app.put('/editarcliente', (req,res)=>{  //nao é async pq precisa passar pela requisição
+    cliente.update(req.body,{
+        where: {id: req.body.id}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: "Cliente foi alterado com sucesso."
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Erro na alteração do CLiente."
+        });
+    });
+});
+
+
+// 3 Utilize a rota para consultar pedidos e faça a edição de um
+//   pedido pelo método put.
+
+//consultar: /listapedidos
+app.put('/editarpedido', (req,res)=>{
+    pedido.update(req.body,{
+        where: {ServicoId: req.body.ServicoId}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: "Pedido modificado com sucesso."
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Não foi possível modificar o pedido."
+        });
+    });
+});
+
+
+
+//destroy
+
+app.get('/excluircliente', async(req,res)=>{
+    await cliente.destroy({
+        where: {id:1}
+    }).then(function(){
+        return res.json("Deletado.");
+    });
+});
+
+app.delete('/apagarcliente/:nome', (req,res)=>{
+    cliente.destroy({
+        where: {nome: req.params.nome}
+    }).then(function(){
+        return res.json({
+            error:false,
+            message: "Cliente foi excluído com sucesso."
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Não foi possível excluir o cliente."
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// port
 let port=process.env.PORT || 3000;
 
 app.listen(port,(req,res)=>{
